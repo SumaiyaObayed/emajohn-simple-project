1ce942f98923e2dc09d0c7dc53d5cabe13ffec00
SumaiyaObayed <sumaiyaobayed99@gmail.com>
2023-03-29 00:14:41 +0600

    button added

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -5,12 +5,21 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts]= useState([]);
+    const [cart, setCart] = useState([])
+
+
     useEffect( () =>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
 
     }, [])
+
+    const handleAddToCart = (product) => {
+        //cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -18,11 +27,13 @@ const Shop = () => {
                     products.map(product => <Products
                     key={product.id}
                     product={product}
+                    handleAddToCart={handleAddToCart}
                     ></Products>)
                 }
             </div>
             <div className='cart-container'>
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
             
         </div>
